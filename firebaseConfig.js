71b0fced7b1bd3e4864fa5094c1831f481440869
Keizer27 <akinpeluusman2018@gmail.com>
2025-06-28 22:06:53 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 // firebaseConfig.js
 export const firebaseConfig = {
   apiKey: "AIzaSyA0EZVBqBGgTOHn6YqIJfmdTmOVJp8n4gc",
-  authDomain: "infopublic-5x9fb.firebaseapp.com",
-  projectId: "infopublic-5x9fb",
-  storageBucket: "infopublic-5x9fb.appspot.com",
+  authDomain: "infoPulse-5x9fb.firebaseapp.com",
+  projectId: "infoPulse-5x9fb",
+  storageBucket: "infoPulse-5x9fb.appspot.com",
   messagingSenderId: "5689711e991",
   appId: "1:568547160991:web:75f63f78408500a2d883aa"
 };

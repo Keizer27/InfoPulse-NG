975858390a8477f9d3ad273ab7a83d4a186e905b
Keizer27 <akinpeluusman2018@gmail.com>
2025-06-29 00:58:25 +0100

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// firebaseConfig.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyA0EZVBqBGgTOHn6YqIJfmdTmOVJp8n4gc",
-  authDomain: "infoPulse-5a276.firebaseapp.com",
-  projectId: "infoPulse-5a276",
-  storageBucket: "infoPulse-5a276.appspot.com",
-  messagingSenderId: "5689711e991",
-  appId: "1:568547160991:web:75f63f78408500a2d883aa"
-};
